37e70e7030ff126923c52ab1799ac607bebb69a0
kkdaini <kkdaini@outlook.com>
2021-09-28 10:03:28 +0800

    创建项目成功！

---
+++ src/test/java/com/kkdaini/wisdom/WisdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkdaini.wisdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisdomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
